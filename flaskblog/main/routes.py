294b7236c55c1a3e66f152d8e5acd091af286db0
Janne K <44394305+jkerola@users.noreply.github.com>
2020-01-20 19:38:18 +0200

    Finished layout design, added nav-links, searchbar, icons and fixed copyright.

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -1,5 +1,5 @@
 '''routes for the main package'''
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for
 
 #blueprint for all general routes
 main = Blueprint('main', __name__)
@@ -9,3 +9,8 @@ main = Blueprint('main', __name__)
 def home():
     '''render home page'''
     return render_template('home.html')
+
+@main.route("/profile")
+def profile():
+    '''render the porfolio page'''
+    return render_template('profile.html', title='Profile')
